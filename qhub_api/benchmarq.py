484768d8b5e19802c1b0b4894c6dbeb75a740b4a
metasuit <116571620+ccchristoph@users.noreply.github.com>
2023-12-24 00:07:13 +0100

    retrieve more info

--- qhub_api/benchmarq.py
+++ qhub_api/benchmarq.py
@@ -1,7 +1,5 @@
 from qiskit_ibm_runtime import Session, IBMBackend, QiskitRuntimeService, RuntimeJob
 from typing import Optional, Union
-
-
 # TODO: use the token validation in another class so that it is faster to benchmark
 
 class Benchmarq:
@@ -12,12 +10,36 @@ class Benchmarq:
     ):
         """Initialize a Benchmarq object."""
         self._job: RuntimeJob = None
+        self._job_id: str = None
+        self._inputs: dict = None
+        self._metrics: dict = None
+        self._result: dict = None
 
         if isinstance(job, RuntimeJob):
             self._job = job
-            return
+            print("deepcopy successful")
         else:
             raise TypeError("Job must be of type RuntimeJob")
+        
+        self._job_id = self._job.job_id()
+        print("Job ID: ", self._job_id)
+
+        self._inputs = self._job.inputs
+        print("Inputs: ", self._inputs)
+
+        self._job.wait_for_final_state()
+        self._result = self._job.result()
+        print("Result: ", self._result)
+
+        self._metrics = self._job.metrics
+        print("Metrics: ", self._metrics)
+
+
+        
+
+
+
+
     
 
         

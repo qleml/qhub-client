331417b041e972a2f736a8e2148f980905e3544d
metasuit <116571620+ccchristoph@users.noreply.github.com>
2023-12-26 23:44:01 +0100

    benchmarq method added

--- qhub_client/client.py
+++ qhub_client/client.py
@@ -1,10 +1,13 @@
 import requests
 from typing import Any, Dict
+from qiskit_ibm_runtime import  RuntimeJob
+from qhub_client.benchmarq import Benchmarq
+
 
 class QhubClient:
     """Qhub Client"""
 
-    def __init__(self, token: str, base_url: str = "http://localhost:3000/api"):
+    def __init__(self, token: str = None, base_url: str = "http://localhost:3000/api"):
         """
         Initialize the QhubClient.
 
@@ -14,39 +17,26 @@ class QhubClient:
         """
         self._token = token
         self._base_url = base_url
-        self._headers = {'Authorization': f'Bearer {self._token}'}
+        # TODO: Add authentication
+        # self._headers = {'Authorization': f'Bearer {self._token}'}
 
-    def _make_request(self, method: str, endpoint: str, data: Dict = None, params: Dict = None) -> Any:
-        """
-        Make an HTTP request to a specific endpoint.
 
-        Args:
-            method (str): HTTP method (e.g., 'GET', 'POST').
-            endpoint (str): API endpoint to hit.
-            data (Dict, optional): Data to send in the case of POST/PUT requests.
-            params (Dict, optional): URL parameters for GET requests.
+    def benchmarq(self, job: RuntimeJob):
 
-        Returns:
-            Response from the server.
-        """
-        
-        url = f"{self._base_url}{endpoint}"
-        response = requests.request(method, url, headers=self._headers, json=data, params=params)
+        benchmarq = Benchmarq(job)
 
-        # You can add more sophisticated error handling and response parsing here
-        response.raise_for_status()
-        return response.json()
+        metrics = benchmarq.get_metrics()
+        print(metrics)
+        response = requests.post(self._base_url + "/algs", json=metrics)
+        if response.status_code == 201:
+            print(response.text)
+        else:
+            raise ValueError("The job could not be benchmarked: " + response.status_code + " " + response.text)
 
-    def get_API_Hello_World(self) -> Dict:
-        """
-        Return a submission by its ID.
 
-        Args:
-            submission_id (str): ID of the submission.
 
-        Returns:
-            Dict: Submission object.
-        """
-        return self._make_request("GET", f"/submission/{submission_id}")
 
-    # You can add more methods to interact with other API endpoints
+
+
+
+
